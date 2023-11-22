22d6cafe27681aade90f6eeb65e6b16c7bdc2bf4
teejay009 <tajdjimoh@gmail.com>
2023-11-22 15:16:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Example1 from './Example1';
+import Example2 from './Example2';
+
+function App() {
+  return (
+    <div className="App">
+      <Example2/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Example2.js
@@ -0,0 +1,89 @@
+import { useRef, useCallback } from "react"
+import { confirmAlert } from "react-confirm-alert"
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import Post from "./Post";
+import { useInfiniteQuery } from "react-query";
+import { getPostsPage } from "./api/axios";
+
+
+
+
+const Example2 = () => {
+
+const {fetchNextPage, hasNextPage, isFetchingNextPage, data, status, error} = 
+useInfiniteQuery('/posts', ({pageParam = 1}) => getPostsPage(pageParam), {
+    getNextPageParam:(lastPage, allPages) => {
+        return lastPage.length ? allPages.length + 1 : undefined
+    }
+})
+  
+  const intObserver = useRef();
+
+
+   const lastPostRef = useCallback((post) => {
+     if (isFetchingNextPage) return;
+     if(intObserver.current) intObserver.current.disconnect();
+
+     const confirmLoad = () => {
+      confirmAlert({
+        title: "confirm Load More",
+        message: `Are you sure to load more post?`,
+        buttons:[
+          {
+            label: 'Yes',
+            onClick: () => {
+                fetchNextPage()
+            }
+          },
+          {label: 'No'}
+        ]
+      })
+     }
+     
+     intObserver.current = new IntersectionObserver((post) => {
+      if(post[0].isIntersecting && hasNextPage){
+        console.log('we are near the last post');
+    confirmLoad()
+      }
+      
+     })
+
+     if(post) intObserver.current.observe(post)
+  }, [isFetchingNextPage,fetchNextPage,hasNextPage]);
+
+if (status === 'error') return <p className="center">Error : {error.message}</p>
+  const content = data?.pages.map(pg => {
+    return pg.map((post, i) => {
+        if(pg.length === i + 1) {
+    
+          return <Post ref={lastPostRef} key={post.id} post={post}/>;
+    
+        }
+        return <Post key={post.id} post={post}/>
+    
+        });
+
+  })
+  
+  
+
+  return (
+    <>
+      <h1 id='top'>
+        &infin; Infinite Query &amp; Scroll
+        <br/>
+        &infin; Ex. 2 - React query
+
+      </h1>
+      {content}
+
+      <p className='center'>Loading more posts...</p>
+
+      <p className='center'>
+        <a href='#top'>Back to Top</a>
+      </p>
+    </>
+  )
+}
+
+export default Example2
